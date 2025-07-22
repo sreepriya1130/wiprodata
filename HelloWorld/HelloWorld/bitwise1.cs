4d199b8add75c7fcc74231b690d09b5fd666a02c
sreepriya1130 <makkena.sreepriyasreenadh@gmai.com>
2025-07-22 19:19:28 +0530

    first commit

---
+++ HelloWorld/HelloWorld/bitwise1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class bitwise1
+    {
+        static void Main()
+        {
+            int a = 5, b = 6;
+            Console.WriteLine(a | b);
+            Console.WriteLine(a & b);
+            Console.WriteLine(a ^ b);
+            Console.WriteLine(~a);
+        }
+    }
+}
